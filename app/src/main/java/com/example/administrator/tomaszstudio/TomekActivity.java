9546ad73fe9c4bd19b09d3f419551075e6d2e3bf
Administrator <tomaszjanos@wp.pl>
2018-02-15 16:24:10 +0100

    init

---
+++ app/src/main/java/com/example/administrator/tomaszstudio/TomekActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.tomaszstudio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TomekActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tomek);
+    }
+}
